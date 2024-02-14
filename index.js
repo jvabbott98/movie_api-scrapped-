7020937aa80f4f7c7fb58480b078db843bc15c4a
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-02-14 20:15:49 +0900

    add authentication

--- index.js
+++ index.js
@@ -12,63 +12,10 @@ const express = require('express'),
       uuid = require('uuid');
 
 app.use(bodyParser.json());
-
-// let movies = [
-//     {
-//         title: 'Bernie',
-//         director: {
-//             name: 'Richard Linklater',
-//             bio: 'Richard Stuart Linklater is an American film director, producer, and screenwriter. He is known for making films that deal thematically with suburban culture and the effects of the passage of time',
-//             birth: '1960'
-//         },
-//         genre: {
-//             name: 'Black Comedy',
-//             description: 'Comedy that makes light of subjects generally cosidered taboo or painful like death or grief.'
-//         }
-//     },
-//     {
-//         title: 'Arrival',
-//         director: {
-//             name: 'Denis Villeneuve',
-//             bio: 'Denis Villeneuve is a Canadian filmmaker known for directing several critically acclaimed films, including the thrillers Prisoners (2013) and Sicario (2015), as well as the science fiction films Arrival (2016) and Blade Runner 2049 (2017).',
-//             birth: '1967'
-//         },
-//         genre: {
-//             name: 'Science Fiction',
-//             description: 'Speculative fiction that frequently deals with futuristic concepts like advaced technology, space exploration and science.'
-//         }
-//     },
-//     {
-//         title: 'Secret Life of Walter Mitty',
-//         director: {
-//             name: 'Ben Stiller',
-//             bio: 'Ben Stiller is an American actor, comedian, and filmmaker. He is the son of the comedians and actors Jerry Stiller and Anne Meara.[1] Stiller was a member of a group of comedic actors colloquially known as the Frat Pack.',
-//             birth: '1965'
-//         },
-//         genre: {
-//             name: 'Comedy-drama',
-//             description: 'Combines elements of drama and comedy in a mix of serious, emotional moments and lighthearted humor.'
-//         }
-//     }
-// ]
-
-// let users = [
-//     {
-//         id: 1,
-//         name: 'Tony',
-//         favoriteMovies: [
-
-//         ]
-//     },
-//     {
-//         id: '2',
-//         name: 'Scott',
-//         favoriteMovies: [
-//             'Bernie'
-//         ]
-//     },
-// ]
-
+app.use(bodyParser.urlencoded({ extended: true }));
+let auth = require('./auth')(app);
+const passport = require('passport');
+require('./passport');
 
 //Create a new user
 app.post('/users', async (req, res) => {
@@ -98,28 +45,33 @@ app.post('/users', async (req, res) => {
   });
 
 // Update a user's info, by username
-app.put('/users/:Username', async (req, res) => {
-    await Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  // CONDITION TO CHECK ADDED HERE
+  if(req.user.Username !== req.params.Username){
+      return res.status(400).send('Permission denied');
+  }
+  // CONDITION ENDS
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+      $set:
       {
-        Username: req.body.Username,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
       }
-    },
-    { new: true }) // This line makes sure that the updated document is returned
-    .then((updatedUser) => {
-      res.json(updatedUser);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    })
-  
-  });
+  },
+      { new: true }) // This line makes sure that the updated document is returned
+      .then((updatedUser) => {
+          res.json(updatedUser);
+      })
+      .catch((err) => {
+          console.log(err);
+          res.status(500).send('Error: ' + err);
+      })
+});
 
 //Add new movie to a user's favorite movies list
-app.post('/users/:username/movies/:MovieID', async (req, res) => {
+app.post('/users/:username/movies/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Users.findOneAndUpdate({ Username: req.params.username }, {
        $push: { FavoriteMovies: req.params.MovieID }
      },
@@ -134,7 +86,7 @@ app.post('/users/:username/movies/:MovieID', async (req, res) => {
   });
 
 //Remove movie from user's favoite movies list
-app.delete('/users/:username/movies/:MovieID', async (req, res) => {
+app.delete('/users/:username/movies/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Users.findOneAndUpdate({ Username: req.params.username }, {
        $pull: { FavoriteMovies: req.params.MovieID }
      },
@@ -149,7 +101,7 @@ app.delete('/users/:username/movies/:MovieID', async (req, res) => {
   });
 
 // Get all users
-app.get('/users', async (req, res) => {
+app.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Users.find()
       .then((users) => {
         res.status(201).json(users);
@@ -161,7 +113,7 @@ app.get('/users', async (req, res) => {
   });
 
   // Get a user by username
-app.get('/users/:Username', async (req, res) => {
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Users.findOne({ username: req.params.Username })
       .then((user) => {
         res.json(user);
@@ -173,7 +125,7 @@ app.get('/users/:Username', async (req, res) => {
   });
 
 // Delete a user by username
-app.delete('/users/:Username', async (req, res) => {
+app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Users.findOneAndDelete({ Username: req.params.Username })
       .then((user) => {
         if (!user) {
@@ -190,10 +142,7 @@ app.delete('/users/:Username', async (req, res) => {
 
 
 //Send list of movie data to user
-                                // app.get('/movies', (req, res) => {
-                                //     res.status(200).json(movies);
-                                // });
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
@@ -207,7 +156,7 @@ app.get('/movies', async (req, res) => {
 
 
 //Send data of a single movie to user
-app.get('/movies/:Title', async (req, res) => {
+app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ title: req.params.Title })
     .then((user) => {
       res.json(user);
@@ -219,7 +168,7 @@ app.get('/movies/:Title', async (req, res) => {
 });
 
 //Send data about the genre of a movie to the user
-app.get('/movies/:Genre', async (req, res) => {
+app.get('/movies/:Genre', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ "genre.name" : req.params.Genre })
     .then((user) => {
       res.json(user);
@@ -231,7 +180,7 @@ app.get('/movies/:Genre', async (req, res) => {
 });
 
 //Send data about the director of a movie back to the user
-app.get('/movies/:Director', async (req, res) => {
+app.get('/movies/:Director', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find({ director : req.params.Director })
     .then((user) => {
       res.json(user);
