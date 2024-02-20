91a6e688b7f23aeec5d0738592b039c6b269e664
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-02-20 21:37:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -179,6 +179,16 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), async (req,
     });
 });
 
+app.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 
 //Send data of a single movie to user
